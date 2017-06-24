0a6fa371ff2a272bd451116046f3e07b3b72d878
Massimiliano Botticelli <mabogs00@hs-esslingen.de>
2017-06-24 22:25:10 +0200

    senseHat folder cleaned up

--- CMakeFiles/comminication.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libcomminication.pdb"
-  "libcomminication.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/comminication.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
